ba13714f04df80ac18565b68e143ffcd613c09fc
scott wright <scotty.wright30@gmail.com>
2020-11-27 10:59:35 +0000

    init

--- Catalog.API/Controllers/CatalogController.cs
+++ Catalog.API/Controllers/CatalogController.cs
@@ -1,13 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace Catalog.API.Controllers
 {
-    public class CatalogController : Controller
+    public class CatalogController : ControllerBase
     {
-        // GET
-        public IActionResult Index()
-        {
-            return View();
-        }
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Name")]
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Summary { get; set; }
+        public string Description { get; set; }
+        public string ImageFile { get; set; }
+        public decimal Price { get; set; }
     }
 }
