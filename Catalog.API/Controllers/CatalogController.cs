a93ce3b322cba777e735b0e4af4e9178567216cc
scott wright <scotty.wright30@gmail.com>
2020-11-27 11:01:21 +0000

    Add entity

--- Catalog.API/Controllers/CatalogController.cs
+++ Catalog.API/Controllers/CatalogController.cs
@@ -1,21 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
-using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
 
 namespace Catalog.API.Controllers
 {
     public class CatalogController : ControllerBase
     {
-        [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string Id { get; set; }
-
-        [BsonElement("Name")]
-        public string Name { get; set; }
-        public string Category { get; set; }
-        public string Summary { get; set; }
-        public string Description { get; set; }
-        public string ImageFile { get; set; }
-        public decimal Price { get; set; }
     }
 }
